8ef9ce521df3dc3350cdb1c42cbf5a171687c331
pierre <pierre.housseaux54@gmail.com>
2022-11-01 16:43:17 +0100

    modification of color_detection.py - first iteration of the Hough Transforms

--- modules/color_detection/color_detection.py
+++ modules/color_detection/color_detection.py
@@ -70,7 +70,7 @@ def apply_mask(img, lower, upper):
     return mask
 
 
-def find_blue_ball():
+def find_blue_ball(print_all=False):
     # Initializing the webcam
 
     wCam, hCam = 2340, 1920  # Parameters from camera
@@ -88,12 +88,15 @@ def find_blue_ball():
     cv2.resizeWindow("TrackBars", 640, 290)
     cv2.createTrackbar("Hue Min", "TrackBars", 103, 179, empty)
     cv2.createTrackbar("Hue Max", "TrackBars", 170, 179, empty)
-    cv2.createTrackbar("Sat Min", "TrackBars", 154, 255, empty)
+    cv2.createTrackbar("Sat Min", "TrackBars", 103, 255, empty)
     cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
     cv2.createTrackbar("Val Min", "TrackBars", 77, 255, empty)
     cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
-    cv2.createTrackbar("Param 1", "TrackBars", 50, 250, empty)
-    cv2.createTrackbar("Param 2", "TrackBars", 25, 250, empty)
+    cv2.createTrackbar("Sensitivity: Circles detection", "TrackBars", 100, 300, empty)
+    cv2.createTrackbar("Accuracy: Circles detection", "TrackBars", 15, 25, empty)
+
+    prev_circle = None
+    dist = lambda x1, y1, x2, y2: (x1 - x2) ** 2 + (y1 - y2) ** 2
 
     while True:
         # Read the camera flow
@@ -124,27 +127,51 @@ def find_blue_ball():
         Finding the circle 
         """
 
-        mask_blur = cv2.medianBlur(mask, 5)     #mask already in grayscale
+        mask_blur = cv2.medianBlur(mask, 7)  # mask already in grayscale
 
-        param1 = cv2.getTrackbarPos("Param 1", "TrackBars")
-        param2 = cv2.getTrackbarPos("Param 2", "TrackBars")
-        circles = cv2.HoughCircles(mask_blur, cv2.HOUGH_GRADIENT, 1, 20,
+        param1 = cv2.getTrackbarPos("Sensitivity: Circles detection", "TrackBars")
+        param2 = cv2.getTrackbarPos("Accuracy: Circles detection", "TrackBars")
+        circles = cv2.HoughCircles(mask_blur, cv2.HOUGH_GRADIENT, 1, 100,
                                    param1=param1, param2=param2, minRadius=0, maxRadius=0)
 
         if circles is not None:
             detected_circles = np.uint16(circles).reshape((circles.shape[1], circles.shape[2]))
 
-            for (x, y, r) in detected_circles:
-                # draw the outer circles
-                print(x, y, r)
-                cv2.circle(img_copy, (x, y), r, (0, 255, 0), 2)
-                # draw the center of the circle
-                cv2.circle(img_copy, (x, y), 2, (0, 255, 255), 3)
-        else:
-            print("No balls found")
-            # Plot the resulting mask
+            # We want to filter all circles that was not close to the previous one
 
-        frame_img=cv2.hconcat((img, img_copy))
+            closest_circle = None
+
+            for circle in detected_circles:
+                if closest_circle is None: closest_circle = circle
+                if prev_circle is not None:
+
+                    if np.linalg.norm(closest_circle[:2]-prev_circle[:2]) <= np.linalg.norm(circle[:2]-prev_circle[:2]):
+                        closest_circle = circle
+
+            x_closet, y_closet, r_closet = closest_circle
+            cv2.circle(img_copy, (x_closet, y_closet), r_closet, (0, 0, 255), 6)
+            cv2.circle(img_copy, (x_closet, y_closet), 2, (0, 255, 255), 3)
+            print(x_closet, y_closet, r_closet)
+            """ 
+            # Since we have reflection on the light on the ball it generate sometimes additional circles
+            # we only want to keep track of the largest circles detected
+
+            circle_max = np.max(detected_circles, axis=0)
+            x_max, y_max, r_max = circle_max
+            cv2.circle(img_copy, (x_max, y_max), r_max, (255, 0, 0), 4)
+            cv2.circle(img_copy, (x_max, y_max), 2, (0, 255, 255), 3)
+            """
+
+            prev_circle = closest_circle
+
+            if print_all:
+                for (x, y, r) in detected_circles:
+                    # draw the outer circles
+                    cv2.circle(img_copy, (x, y), r, (0, 255, 0), 2)
+                    # draw the center of the circle
+                    cv2.circle(img_copy, (x, y), 2, (0, 255, 255), 3)
+
+        frame_img = cv2.hconcat((img, img_copy))
         frame_mask = cv2.hconcat((mask, mask_blur))
 
         cv2.imshow("Mask with median blur applied", frame_mask)
@@ -154,6 +181,7 @@ def find_blue_ball():
             break
 
     return lower, upper, mask
+
 
 # lower = np.array([37, 137, 26])
 # upper = np.array([133, 255, 255])

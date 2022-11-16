010cfa6daaa920be9036bd74b03904de51719758
pierre <pierre.housseaux54@gmail.com>
2022-11-16 17:25:00 +0100

    -added mqqt functions

--- modules/mqtt/mqtt_publish.py
+++ modules/mqtt/mqtt_publish.py
@@ -16,8 +16,14 @@ client = mqtt.Client("Client_pub")
 
 client.connect(broker, broker_port)
 
+name_robot="Robot1"
 while True:
     number = random.randint(0, 10)
-    client.publish("Test_mqtt/randnumber", number)
-    client.publish("Test_mqtt/string", "Pierre")
+    client.publish(f"Chenille-based-learning/{name_robot}/Leader", 1)
+    client.publish(f"Chenille-based-learning/{name_robot}/x", number)
+    client.publish(f"Chenille-based-learning/{name_robot}/y", number+1)
+    client.publish(f"Chenille-based-learning/{name_robot}/ball/d_robot", number + 1)
+    client.publish(f"Chenille-based-learning/{name_robot}/ball/dx_cam", number + 1)
+    client.publish(f"Chenille-based-learning/{name_robot}/ball/dy_cam", number + 1)
+    client.publish(f"Chenille-based-learning/{name_robot}/ball/orientation", number + 1)
     time.sleep(1)

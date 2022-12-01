8d1ee70ada9c8688687ab17515db5dda1b1294e4
RobKenobi <robinson.vaitilingom@outlook.fr>
2022-12-01 22:39:06 +0100

    updated test_processing_function

--- ARDUINO/test/test_comm_pi_arduino_ordi/test_comm_pi_arduino_ordi.ino
+++ ARDUINO/test/test_comm_pi_arduino_ordi/test_comm_pi_arduino_ordi.ino
@@ -1,6 +1,6 @@
 void setup(){
   Serial.begin(115200); // Serial connection with computer
-  Serial1.begin(9600); // Serial connection with raspberry
+  Serial1.begin(115200); // Serial connection with raspberry
   // Checking whether the raspeberry is connected
   while(!Serial1){
     Serial.println("Waiting for connection with raspberry"); // Printing message on connected computer

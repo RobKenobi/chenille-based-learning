d90b4dd0aff93ac9d3848aa53a3e4638c41948e8
Mlk810 <malak.kojok@ipsa.fr>
2022-12-01 22:48:05 +0100

    Changed python code for sending data to the arduino

--- test/Comm_python_arduino.py
+++ test/Comm_python_arduino.py
@@ -4,6 +4,5 @@ import time
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
 
 while True:
-    value = arduino.write(b"0.9;12.3")
-    time.sleep(0.05)
-    print(value)
+    value = arduino.write(b"0.9;12.3\n")
+    time.sleep(1)

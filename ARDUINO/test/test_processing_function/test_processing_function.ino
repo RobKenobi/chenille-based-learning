8ea8d6b7ae7fc9db78d15450a84fb259b6dbd481
RobKenobi <robinson.vaitilingom@outlook.fr>
2022-12-02 10:33:59 +0100

    update test file for processing

--- ARDUINO/test/test_processing_function/test_processing_function.ino
+++ ARDUINO/test/test_processing_function/test_processing_function.ino
@@ -1,16 +1,18 @@
 
 // Definition of the struct containing the information transmitted by the raspberry
-struct ErrorInput{
+struct ParsedInput{
   float heading_error;  // Heading error computed by the raspberry
   float distance_error; // Distance error computed by the raspberry
 };
 
 
-ErrorInput processReceivedData(const String message){
-  ErrorInput error;
-  int sep = message.indexOf(";");
-  error.heading_error = message.substring(0,sep).toFloat();
-  error.distance_error = message.substring(sep + 1).toFloat();
+ParsedInput processReceivedData(const String message){
+  // This function allows us to process the message received with the Serial communication
+  ParsedInput data; // Struct which will contain the information extracted from the message
+  int sep = message.indexOf(";"); // This the separator of the data in the message
+  data.heading_error = message.substring(0,sep).toFloat(); // Retrieving first information in the message
+  data.distance_error = message.substring(sep + 1).toFloat(); // Retrieving second information in the message
+  return data;
 }
 
 
@@ -25,8 +27,7 @@ void loop() {
   if (Serial1.available()){
       message = Serial1.readStringUntil('\n');  // Read the data transmitted until the character RL is found
       message.trim();
-      Serial.println("Message" + message);
-      ErrorInput error = processReceivedData(message);
+      ParsedInput error = processReceivedData(message);
       Serial.println("Received : " + String(error.heading_error) + " ; " + String(error.distance_error));
   delay(1000);
   }

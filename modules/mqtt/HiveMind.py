2780d3cd31828276b8ae7ddddb5dcde2b9c330a7
pierre <pierre.housseaux54@gmail.com>
2022-12-13 14:20:33 +0100

    -communication updates

--- modules/mqtt/HiveMind.py
+++ modules/mqtt/HiveMind.py
@@ -1,25 +1,37 @@
 import time
 import paho.mqtt.client as mqtt
-from multiprocessing import Process
+import json
+import threading
 
 Connected = False
+leader = 0
+population = []
 
+sem = threading.Semaphore()
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to the broker")
         global Connected
         Connected = True
+        client.publish("Chenille-based-learning/HiveMind/population", 0, qos=2, retain=True)
+        client.publish("Chenille-based-learning/HiveMind/leader", leader, qos=1, retain=True)
     else:
         print("Connection failed")
 
-
 def on_message(client, userdata, message):
-    print("oh")
-    print("message received ", str(message.payload.decode("utf-8")))
-    print("message topic=", message.topic)
-    print("message qos=", message.qos)
-    print("message retain flag=", message.retain)
+    population.append(json.loads(message.payload))
+    # print("message topic=", message.topic)
+    # print("message qos=", message.qos)
+    # print("message retain flag=", message.retain)
+
+
+def check_population(client, userdata, message):
+    print("Current population:", str(message.payload.decode()))
+
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected to the broker")
 
 
 broker = "broker.hivemq.com"
@@ -28,25 +40,28 @@ broker_port = 1883
 client = mqtt.Client("HiveMind", clean_session=False)
 
 client.on_connect = on_connect
-client.on_message = on_message
+
+# client.message_callback_add()
+client.message_callback_add("Chenille-based-learning/HiveMind/population", check_population)
+client.message_callback_add("Chenille-based-learning/Swarm/#", on_message)
+# client.on_message = on_message
+# client.on_publish = on_publish
+client.on_disconnect = on_disconnect
 
 client.connect(broker, broker_port, keepalive=60)
 
 client.loop_start()  # Start the loop
 
 while Connected != True:  # Wait for the client to connect
+    print(population)
     time.sleep(1)
 
-client.subscribe("publisher.connect/#")
+client.subscribe("Chenille-based-learning/Swarm/#")
 
 try:
     while True:
-        client.publish("Chenille-based-learning/HiveMind/number_of_chenille", 1)
-
         time.sleep(1)
 
 except KeyboardInterrupt:
-    print("Disconnecting from the broker ...")
     client.disconnect()
     client.loop_stop()
-    print("Disconnected from the broker")

86d7fffc6bea47c5a678c2878050baa080199c46
RobKenobi <robinson.vaitilingom@outlook.fr>
2022-12-13 14:38:01 +0100

    new main for raspberry

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import time
 
 import cv2
 import serial
-from picamera import PiCamera
-from picamera.array import PiRGBArray
 
 from modules.ball_detection import BallDetector
 from modules.ball_tracking import BallTracker
@@ -20,40 +18,44 @@ height_reference = 0.5
 # Ball tracker
 tracker = BallTracker(height_reference=height_reference, tolerance=tolerance)
 
-# Initialize the Pi camera
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+# Video feed
+cap = cv2.VideoCapture(0)
 
 # Wait for the camera to be ready
 time.sleep(0.1)
 
-try:
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = rawCapture.array
+while True:
+    # Press <ESC>
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        cap.release()
+        break
 
-        if image is not None:  # if an image is detected
-            # Flipping image
-            image = cv2.flip(image, -1)
-            cv2.imwrite("test_image.png", image)
-            # Trying to detect the ball
-            success, target = detector.detect_ball(image)
-            print(success)
-            if success:
-                deviation = tracker.get_deviation(image, target)
-                radius = target[-1]
+    # Getting image from camera
+    _, image = cap.read()
 
-                # TODO : find the target radius
-                target_radius = 10
+    if image is not None:
+        # Flipping image
+        image = cv2.flip(image, -1)
 
-                # TODO : find the reduction factor
-                heading_error = deviation[1]
-                distance_error = target_radius - radius
+        # Trying to detect the ball
+        success, target = detector.detect_ball(image)
 
-                # TODO : send infos to MQTT server
-                # TODO : 
-                # TODO : send the error to the arduino
-        rawCapture.truncate()
-        rawCapture.seek(0)
+        if success:
+            x_closet, y_closet, r_closet = target
+            cv2.circle(image, (x_closet, y_closet), r_closet, (0, 0, 255), 6)
+            cv2.circle(image, (x_closet, y_closet), 2, (0, 255, 255), 3)
 
-except KeyboardInterrupt:
-    camera.close()
+            deviation = tracker.get_deviation(image, target)
+            radius = target[-1]
+
+            # TODO : find the target radius
+            target_radius = 10
+
+            # TODO : find the reduction factor
+            heading_error = deviation[1]
+            distance_error = target_radius - radius
+
+            # TODO : send infos to MQTT server
+            # TODO :
+            # TODO : send the error to the arduino

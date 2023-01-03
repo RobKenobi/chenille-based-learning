46922f1b4690abf7ec03304e7912782177a2453e
RobKenobi <robinson.vaitilingom@outlook.fr>
2023-01-03 15:03:52 +0100

    created main for aruco following

---
+++ main_aruco_only.py
@@ -0,0 +1,54 @@
+import time
+
+import cv2
+# import serial
+
+from modules.aruco import ArucoDetector
+
+# Communication with Arduino
+# serialArduino = serial.Serial("/dev/ttyUSB0", 115200)
+
+# Aruco Detection
+detector = ArucoDetector()
+
+# Video feed
+cap = cv2.VideoCapture(0)
+
+# Wait for the camera to be ready
+time.sleep(0.1)
+
+last_time = time.time()
+
+while True:
+    # Press <ESC>
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        cap.release()
+        break
+
+    # Getting image from camera
+    ret, image = cap.read()
+
+    if ret:
+        # Convert BRG image to GRAY image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Flipping image
+        image = cv2.flip(image, -1)
+
+        # Trying to detect the ball
+        success, *_ = detector.detection(gray)
+
+        if success and time.time() - last_time > 0.5:
+            heading_error, distance_error = detector.get_deviation()
+            servo = 0
+
+            message = f"Distance error : {distance_error}\nHeading error : {heading_error}\nServo : {servo}"
+            print(10 * "---")
+            print(message)
+
+            message_to_send = f"{distance_error};{heading_error};{servo}\n"
+            # serialArduino.write(message_to_send.encode())
+            last_time = time.time()
+
+        cv2.imshow("Image", image)

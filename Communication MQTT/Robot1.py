557e7e9097b2368c50d66c62b267e0cca8d1493a
Mlk810 <malak.kojok@ipsa.fr>
2023-01-03 12:00:26 +0100

    New file with communication MQTT done

---
+++ Communication MQTT/Robot1.py
@@ -0,0 +1,82 @@
+import time
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import json
+import random
+
+Connected = False
+
+i = None
+
+status = -1
+
+
+def update_population(client, userdata, message):
+    global i
+    i = int(message.payload.decode())
+
+
+def get_status(client, userdata, message):
+    global status
+    status = int(message.payload.decode())
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to the broker")
+        global Connected
+        Connected = True
+        client.subscribe("Chenille-based-learning/Server/population")
+        client.subscribe("Chenille-based-learning/Robots/#")
+    else:
+        print("Connection failed")
+
+
+broker = "broker.hivemq.com"
+broker_port = 1883
+
+name_robot = "Robot1"
+client = mqtt.Client(name_robot, clean_session=True)
+
+# Callbacks
+client.on_connect = on_connect
+
+client.message_callback_add("Chenille-based-learning/Server/population", update_population)
+client.message_callback_add(f"Chenille-based-learning/Robots/{name_robot}/status", get_status)
+
+client.connect(broker, broker_port, keepalive=10)
+
+client.loop_start()  # Start the loop
+
+while Connected != True:  # Wait for the client to connect
+    time.sleep(1)
+
+client.publish("Chenille-based-learning/Server/population", i + 1, qos=2, retain=True)
+# client.will_set("Chenille-based-learning/HiveMind/population", i - 1, qos=2, retain=True)
+
+# data = {"Name": name_robot}  # -1: waiting for instructions from the server 0: follower 1:leader
+
+client.publish(f"Chenille-based-learning/Robots/{name_robot}/status", status, qos=1)
+
+try:
+    while True:
+        target = random.randint(3, 9)  # Radius from ball
+
+        # data["Target"] = target
+        # data_json = json.dumps(data)
+        # client.publish(f"Chenille-based-learning/Swarm/{name_robot}", data_json, qos=1)
+        client.publish(f"Chenille-based-learning/Robots/{name_robot}/BallRadius", target, qos=1)
+
+        print(status)
+        if status == 1:
+            print("I am the Leader")
+        else:
+            print("I am the follower")
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Disconnecting from the broker ...")
+    client.publish("Chenille-based-learning/Server/population", i - 1, qos=2, retain=True)
+    client.disconnect()
+    client.loop_stop()
---
+++ Communication MQTT/Server.py
@@ -0,0 +1,86 @@
+import time
+import paho.mqtt.client as mqtt
+import json
+import threading
+import numpy as np
+
+Connected = False
+
+radius = np.zeros((2, 1))
+names = ["Robot1", "Robot2"]
+
+sem = threading.Semaphore()
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to the broker")
+        global Connected
+        Connected = True
+        client.publish("Chenille-based-learning/Server/population", 0, qos=2, retain=True)
+    else:
+        print("Connection failed")
+
+
+def on_message(client, userdata, message):
+    # data = json.loads(message.payload)
+    data = message.payload.decode()
+    # data.pop
+    # data.append
+    topic_name = message.topic
+    if topic_name[30:-7] == "Robot1":
+        radius[0] = data
+    if topic_name[30:-7] == "Robot2":
+        radius[1] = data
+
+
+def check_population(client, userdata, message):
+    print("Current population:", str(message.payload.decode()))
+
+
+def on_disconnect(client, userdata, rc):
+    print("Disconnected to the broker")
+
+
+broker = "broker.hivemq.com"
+broker_port = 1883
+
+client = mqtt.Client("Server", clean_session=False)
+
+client.on_connect = on_connect
+
+client.message_callback_add("Chenille-based-learning/Server/population", check_population)
+client.message_callback_add("Chenille-based-learning/Robots/+/BallRadius", on_message)
+
+client.on_disconnect = on_disconnect
+
+client.connect(broker, broker_port, keepalive=60)
+
+client.loop_start()  # Start the loop
+
+while Connected != True:  # Wait for the client to connect
+    time.sleep(1)
+
+client.subscribe("Chenille-based-learning/Robots/#")
+
+try:
+    while True:
+        followers = names.copy()
+        leader = followers[np.argmax(radius)]
+
+        followers.remove(leader)
+
+        if np.max(radius) != 0:
+            print("Le leader est ", leader)
+
+            client.publish(f"Chenille-based-learning/Robots/{leader}/status", 1, qos=1, retain=True)
+            for follower in followers:
+                client.publish(f"Chenille-based-learning/Robots/{follower}/status", 0, qos=1, retain=True)
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    for name in names:
+        client.publish(f"Chenille-based-learning/Robots/{name}/status", -1, qos=1)
+    client.disconnect()
+    client.loop_stop()

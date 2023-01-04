a3dc2b99ad670b1763f318120090053b2265db42
RobKenobi <robinson.vaitilingom@outlook.fr>
2023-01-04 18:12:03 +0100

    reduced reduction factor for heading with ball following

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ try:
                     target_radius = 70  # The robot should be approximately at 20 cm of the ball
 
                     # Computing error with reduction factors
-                    heading_error = - deviation[0] / 200
+                    heading_error = - deviation[0] / 175
                     distance_error = (target_radius - radius) / 70
 
                 # FOLLOWER

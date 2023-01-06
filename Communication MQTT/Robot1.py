dbd42a5fce308abcdb5693d86eb285f9eb642e70
RobKenobi <robinson.vaitilingom@outlook.fr>
2023-01-06 11:16:46 +0100

    update Robot1.py and modified reduction factor of main_ball_only.py to match those in main.py

--- Communication MQTT/Robot1.py
+++ Communication MQTT/Robot1.py
@@ -5,16 +5,6 @@ import paho.mqtt.client as mqtt
 
 Connected = False
 
-i = None
-
-status = -1
-
-
-def update_population(client, userdata, message):
-    global i
-    print("update pop ", i)
-    i = int(message.payload.decode())
-
 
 def get_status(client, userdata, message):
     global status
@@ -26,7 +16,6 @@ def on_connect(client, userdata, flags, rc):
         print("Connected to the broker")
         global Connected
         Connected = True
-        client.subscribe("Chenille-based-learning/Server/population")
         client.subscribe("Chenille-based-learning/Robots/#")
     else:
         print("Connection failed")
@@ -41,9 +30,6 @@ client = mqtt.Client(name_robot, clean_session=True)
 # Callbacks
 client.on_connect = on_connect
 
-client.message_callback_add("Chenille-based-learning/Server/population", update_population)
-client.message_callback_add(f"Chenille-based-learning/Robots/{name_robot}/status", get_status)
-
 client.connect(broker, broker_port, keepalive=10)
 
 client.loop_start()  # Start the loop
@@ -51,14 +37,14 @@ client.loop_start()  # Start the loop
 while not Connected:  # Wait for the client to connect
     time.sleep(1)
 
-client.publish("Chenille-based-learning/Server/population", i + 1, qos=2, retain=True)
-# -1: waiting for instructions from the server 0: follower 1:leader
+client.message_callback_add(f"Chenille-based-learning/Robots/{name_robot}/status", get_status)
 
+status = -1
 client.publish(f"Chenille-based-learning/Robots/{name_robot}/status", status, qos=1)
 
 try:
     while True:
-        target = random.randint(3, 9)  # Radius of ball
+        target = random.randint(1000, 2000)  # Radius of ball
 
         client.publish(f"Chenille-based-learning/Robots/{name_robot}/BallRadius", target, qos=1)
 
@@ -74,6 +60,5 @@ try:
 
 except KeyboardInterrupt:
     print("Disconnecting from the broker ...")
-    client.publish("Chenille-based-learning/Server/population", i - 1, qos=2, retain=True)
     client.disconnect()
     client.loop_stop()
